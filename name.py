17c08936ed3d1809cdcc38ef4a82f2b42ba63a65
Wang-Sir11 <1710820782@qq.com>
2021-09-22 17:51:23 +0800

    添加一个代码

---
+++ name.py
@@ -0,0 +1,16 @@
+while 1:
+    score = input("please int your score:")
+    score = int(score)
+    if   score > 0 and score <= 20:
+            print("你的等级是E。")
+    elif score > 20 and score <= 40:
+            print("你的等级是D。")
+    elif score > 40 and score <= 60:
+            print("你的等级是C。")
+    elif score > 60 and score <= 80:
+            print("你的等级是B。")
+    elif score > 80 and score <= 100:
+            print("你的等级是A。")    
+    else:
+            print("瞎扯淡")
+print("输入结束")
